b8216d42049ccd2b6da5c0a99e17391a8015e88e
serenasensini <serena.sensini@gmail.com>
2022-05-25 08:34:59 +0200

    forth lesson OK

---
+++ lesson#2/solution1.py
@@ -0,0 +1,7 @@
+numero = 49
+
+if numero % 7 == 0 and numero % 5 != 0:
+    print("La condizione è soddisfatta")
+else:
+    print("La condizione non è soddisfatta")
+
--- lesson#3/main.py
+++ lesson#3/main.py
@@ -40,12 +40,143 @@
 
 # SCRIVERE UNA FUNZIONE CHE STAMPI I NUMERI PARI IN UN ARRAY
 
-lista = [4, 6, 7, 2, 9]
+# lista = [4, 6, 7, 2, 9]
+#
+# def pari(elenco):
+#     for numero in elenco:
+#         if numero % 2 == 0:
+#             print(numero)
+#             # print("è pari")
+#
+# pari(lista)
 
-def pari(elenco):
-    for numero in elenco:
-        if numero % 2 == 0:
-            print(numero)
-            # print("è pari")
+# SOLUZIONE N.1
 
-pari(lista)
+# limite = int(input("Inserisci il limite superiore"))
+#
+# somma = 0
+
+'''
+0+1=1
+1+2=3
+...
+1=0+1
+3=1+2
+...
+risultato = 0
+1=risultato+1
+3=risultato+2
+...
+risultato = 0
+risultato=risultato+1
+risultato=risultato+2
+'''
+
+
+# for numero in range(0, limite, 1):
+#     # somma = somma + numero
+#     somma += numero
+#
+# print(somma)
+
+# SOLUZIONE N.3
+
+# raggio = 4
+#
+# pi_greco = 3.14
+#
+# area = raggio * raggio * pi_greco
+# print(area)
+
+# def circonferenza(raggio):
+#     pi_greco = 3.14
+#     risultato = 2 * pi_greco * raggio
+#     return risultato
+#
+#
+# print(circonferenza(10))
+#
+#
+# def area_cerchio(raggio):
+#     pi_greco = 3.14
+#     area = pi_greco * (raggio ** 2)
+#     return area
+#
+#
+# print(area_cerchio(10))
+
+# SOLUZIONE N.6
+
+'''
+ ****                                                                   
+ *   *                                                                  
+ *   *                                                                  
+ ****                                                                   
+ *                                                                      
+ *                                                                      
+ *  
+'''
+
+# print("****")
+# print("*   *")
+# print("*   *")
+# print("****")
+# print("*")
+# print("*")
+# print("*")
+
+
+# SOLUZIONE N.4
+# import random
+#
+# # estrae un numero intero casuale tra 1 e 8
+# answers = random.randint(1, 8)
+#
+# # STEP 1: chiedi all'utente di inserire una domanda per la palla magica
+#
+# domanda = input("Cosa vuoi chiedere alla palla magica? ")
+#
+# # STEP 2: tramite le istruzioni condizionali, scrivi delle possibili risposte per l'utente
+#
+# while domanda != "exit" or domanda != "":
+#     if answers == 1:
+#         print("Meglio non dirlo ora.")
+#     elif answers == 2:
+#         print("Ne sei sicuro?")
+#     elif answers == 3:
+#         print("Direi proprio di sì.")
+#     elif answers == 4:
+#         print("La mia risposta è no.")
+#     elif answers == 5:
+#         print("Chiedimelo più tardi.")
+#     elif answers == 6:
+#         print("Dovresti rifletterci di più.")
+#     elif answers == 7:
+#         print("Ci puoi contare.")
+#     else:
+#         print("Le mie fonti dicono di sì.")
+#     domanda = input("Cosa vuoi chiedere alla palla magica? ")
+#     answers = random.randint(1, 8)
+
+
+# diz = {"Marco": 1234, "Alessio": 4567, "Claudio": 7894}
+# print(diz)
+#
+# del diz['Marco']
+# print(diz)
+#
+# lista = [1,2,3,4,5]
+# del lista[5]
+# print(lista)
+
+fruits = {"apple", "banana", "cherry"}
+
+fruits.add("pear")
+fruits.add("pear")
+fruits.add("pear")
+
+print(fruits)
+
+fruits.remove("pear")
+
+print(fruits)
